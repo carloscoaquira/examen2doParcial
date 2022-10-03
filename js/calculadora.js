44837951f3443bbc41bffd911bed12129996bac0
Carlos Coaquira <carlosbcoaquirao@gmail.com>
2022-10-02 22:45:00 -0400

    toda la parte principal realizada

--- js/calculadora.js
+++ js/calculadora.js
@@ -17,23 +17,112 @@ const calculadora = new Operaciones();
 const containerCalculadora = document.querySelector('#calculadora');
 let valorInicial = document.querySelector('#valorInicial');
 let valorFinal = document.querySelector('#valorFinal');
+let primeraVariable = 0;
+let segundaVariable = 0;
+let valorOperacion = undefined ;
 containerCalculadora.addEventListener('click', e => {
     if(e.target.tagName == 'BUTTON'){
         let btn = e.target;
-        if(btn.value == 'numero'){
-            if(valorFinal.innerHTML == 0 || valorFinal.innerHTML == null){
-                valorFinal.innerHTML = btn.innerHTML;
-            }else{
-                valorFinal.innerHTML += btn.innerHTML;
-            }
+        switch(btn.value){
+            case 'numero':
+                if(valorFinal.innerHTML == 0 || valorFinal.innerHTML == null){
+                    valorFinal.innerHTML = btn.innerHTML;
+                }else{
+                    valorFinal.innerHTML += btn.innerHTML;
+                }
+                break;
+            case 'operador':
+                if(primeraVariable == 0 && valorOperacion == undefined){
+                    primeraVariable = parseFloat(valorFinal.innerHTML);
+                    valorOperacion = btn.dataset.operacion;
+                    valorInicial.innerHTML = `${primeraVariable} ${btn.innerHTML}`;
+                    valorFinal.innerHTML = " ";
+                    console.log(primeraVariable,typeof valorOperacion);
+                }else if(primeraVariable != 0 && valorOperacion != undefined){
+                    segundaVariable = parseFloat(valorFinal.innerHTML);
+                    if(!isNaN(primeraVariable) && !isNaN(segundaVariable)){
+                        let resultadoInOperation = 0;
+                        switch(valorOperacion){
+                            case 'dividir':
+                                resultadoInOperation = calculadora.dividir(primeraVariable, segundaVariable);
+                                valorInicial.innerHTML = `${resultadoInOperation} ${btn.innerHTML}`;
+                                primeraVariable = resultadoInOperation;
+                                valorOperacion = btn.dataset.operacion;
+                                valorFinal.innerHTML = " ";
+                                segundaVariable = 0;
+                                break;
+                            case 'multiplicar':
+                                resultadoInOperation = calculadora.multiplicar(primeraVariable, segundaVariable);
+                                valorInicial.innerHTML = `${resultadoInOperation} ${btn.innerHTML}`;
+                                primeraVariable = resultadoInOperation;
+                                valorOperacion = btn.dataset.operacion;
+                                valorFinal.innerHTML = " ";
+                                segundaVariable = 0;
+                                break;
+                            case 'restar':
+                                resultadoInOperation = calculadora.restar(primeraVariable, segundaVariable);
+                                valorInicial.innerHTML = `${resultadoInOperation} ${btn.innerHTML}`;
+                                primeraVariable = resultadoInOperation;
+                                valorOperacion = btn.dataset.operacion;
+                                valorFinal.innerHTML = " ";
+                                segundaVariable = 0;
+                                break;
+                            case 'sumar':
+                                resultadoInOperation = calculadora.sumar(primeraVariable, segundaVariable);
+                                valorInicial.innerHTML = `${resultadoInOperation} ${btn.innerHTML}`;
+                                primeraVariable = resultadoInOperation;
+                                valorOperacion = btn.dataset.operacion;
+                                valorFinal.innerHTML = " ";
+                                segundaVariable = 0;
+                                break;
+                        }
+                    }
+                }
+                break;
+            case 'resultado':
+                segundaVariable = parseFloat(valorFinal.innerHTML);
+                if(!isNaN(primeraVariable) && !isNaN(segundaVariable)){
+                    switch(valorOperacion){
+                        case 'dividir':
+                            valorInicial.innerHTML = " ";
+                            valorFinal.innerHTML = `${calculadora.dividir(primeraVariable, segundaVariable)}`;
+                            primeraVariable = 0;
+                            segundaVariable = 0;
+                            valorOperacion = undefined;
+                            break;
+                        case 'multiplicar':
+                            valorInicial.innerHTML = " ";
+                            valorFinal.innerHTML = `${calculadora.multiplicar(primeraVariable, segundaVariable)}`;
+                            primeraVariable = 0;
+                            segundaVariable = 0;
+                            valorOperacion = undefined;
+                            break;
+                        case 'restar':
+                            valorInicial.innerHTML = " ";
+                            valorFinal.innerHTML = `${calculadora.restar(primeraVariable, segundaVariable)}`;
+                            primeraVariable = 0;
+                            segundaVariable = 0;
+                            valorOperacion = undefined;
+                            break;
+                        case 'sumar':
+                            valorInicial.innerHTML = " ";
+                            valorFinal.innerHTML = `${calculadora.sumar(primeraVariable, segundaVariable)}`;
+                            primeraVariable = 0;
+                            segundaVariable = 0;
+                            valorOperacion = undefined;
+                            break;
+                    }
+                }
+                break;
+            case 'limpiar':
+                valorInicial.innerHTML = " ";
+                valorFinal.innerHTML = "0";
+                primeraVariable = 0;
+                segundaVariable = 0;
+                valorOperacion = undefined;
+                break;
+            default:
+                console.log(`Sorry, we are out of.`);
         }
-        if(btn.value == 'operador'){
-            if(!valorFinal.innerHTML == 0){
-                let primeraVariable = valorFinal.innerHTML;
-                valorInicial.innerHTML = `${primeraVariable} ${btn.innerHTML}`;
-                valorFinal.innerHTML = "";
-            }
-        }
-        if(btn == 'resultado'){}
     }
 });
