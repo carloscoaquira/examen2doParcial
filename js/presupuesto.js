e910b85c447bdcbd84a7e9cca4ea1cbb8940b483
Carlos Coaquira <carlosbcoaquirao@gmail.com>
2022-10-04 14:36:44 -0400

    proyecto finalizado

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -1,66 +1,177 @@
-class presupuesto{
-    constructor(monto){
-        this._monto = monto;
-
+class Dato{
+    constructor(descripcion, valor){
+        this._descripcion = descripcion;
+        this._valor = valor;
     }
-
-
-}
-const containerMontoEgreso = document.querySelector('#containerEgresos');
-const containerMontoIngreso = document.querySelector('#containerIngresos');
-
-function crearElemento(tipo, description, monto){
-    if(tipo == 'egreso'){
-        let container = document.createElement('DIV');
-        let itemMonto = `<div class="row border border-danger mt-2 p-3">
-            <div class="col-6">${description}</div>
-            <div class="col-6 text-right text-danger"> - ${monto}</div>
-        </div>`;
-        container.innerHTML = itemMonto;
-        containerMontoEgreso.appendChild(container);
-    }else if(tipo == 'ingreso'){
-        let container = document.createElement('DIV');
-        let itemMonto = `<div class="row border border-primary mt-2 p-3">
-            <div class="col-6">${description}</div>
-            <div class="col-6 text-right text-primary"> + ${monto}</div>
-        </div>`;
-        container.innerHTML = itemMonto;
-        containerMontoIngreso.appendChild(container);
+    get descripcion(){
+        return this._descripcion;
+    }
+    set descripcion(descripcion){
+        this._descripcion = descripcion;
+    }
+    get valor(){
+        return this._valor;
+    }
+    set valor(valor){
+        this._valor = valor;
     }
 }
 
-function agregarDato(){
-    let formPresupuesto = document.forms['form-presupuesto'];
-    let tipo = formPresupuesto['tipoMonto'].value;
-    let description = formPresupuesto['descriptionMonto'].value;
-    let valor = formPresupuesto['valorMonto'].value;
-    if(description != '' && valor != ''){
-        if(tipo == 'egreso'){
-            crearElemento(tipo, description, valor);
-            console.log('egreso'+tipo, description,  valor);
-        }else if(tipo == 'ingreso'){
-            crearElemento(tipo, description, valor);
-            console.log('ingreso'+tipo, description,  valor);
+class Egreso extends Dato{
+    static contadorEgresos = 0;
+
+    constructor(descripcion, valor){
+        super(descripcion, valor);
+        this._id = ++Egreso.contadorEgresos;
+    }
+    get id(){
+        return this._id;
+    }
+}
+
+class Ingreso extends Dato{
+    static contadorIngresos = 0;
+
+    constructor(descripcion, valor){
+        super(descripcion, valor);
+        this._id = ++Ingreso.contadorIngresos;
+    }
+    get id(){
+        return this._id;
+    }
+}
+
+const ingresos = [
+    new Ingreso('Salario', 2100.00),
+    new Ingreso('Venta coche', 1500)
+];
+
+const egresos = [
+    new Egreso('Renta departamento', 900),
+    new Egreso('Ropa', 400)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
+}
+
+let totalIngresos = ()=>{
+    let totalIngreso = 0;
+    for(let ingreso of ingresos){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = ()=>{
+    let totalEgreso = 0;
+    for(let egreso of egresos){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+
+let cargarCabecero = ()=>{
+    let presupuesto = totalIngresos() - totalEgresos();
+    document.getElementById('montoDisponible').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+}
+
+const formatoMoneda = (valor)=>{
+    return valor.toLocaleString('es-ES',{style:'currency', currency:'EUR', minimumFractionDigits:2});
+}
+
+const formatoPorcentaje = (valor)=>{
+    return valor.toLocaleString('en-US',{style:'percent', minimumFractionDigits:2});
+}
+
+const cargarIngresos = ()=>{
+    let ingresosHTML = '';
+    for(let ingreso of ingresos){
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+    document.getElementById('containerIngresos').innerHTML = ingresosHTML;
+}
+
+const crearIngresoHTML = (ingreso)=>{
+    let ingresoHTML = `
+    <div class="elemento limpiarEstilos row bg-info mt-3 p-3 text-white">
+        <div class="elemento_descripcion col-6">${ingreso.descripcion}</div>
+        <div class=" row limpiarEstilos col-6">
+            <div class="elemento_valor col-9">+${formatoMoneda(ingreso.valor)}</div>
+            <div class="elemento_eliminar col-3">
+                <button class='elemento_eliminar--btn btn btn-danger' onclick='eliminarIngreso(${ingreso.id})'>
+                    -
+                </button>
+            </div>
+        </div>
+    </div>
+    `;
+    return ingresoHTML;
+}
+
+const eliminarIngreso = (id)=>{
+    let indiceEliminar = ingresos.findIndex( ingreso => ingreso.id === id);
+    ingresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarIngresos();
+}
+
+const cargarEgresos = ()=>{
+    let egresosHTML = '';
+    for(let egreso of egresos){
+        egresosHTML += crearEgresoHTML(egreso);
+    }
+    document.getElementById('containerEgresos').innerHTML = egresosHTML;
+}
+
+const crearEgresoHTML = (egreso)=>{
+    let egresoHTML = `
+    <div class="elemento limpiarEstilos row bg-danger mt-3 p-3 text-white">
+        <div class="elemento_descripcion col-6">${egreso.descripcion}</div>
+        <div class="row limpiarEstilos col-6">
+            <div class="elemento_valor col-9">-${formatoMoneda(egreso.valor)}</div>
+            <div class="elemento_eliminar col-3">
+                <button class='elemento_eliminar--btn btn btn-primary' onclick='eliminarEgreso(${egreso.id})'>
+                    -
+                </button>
+            </div>
+        </div>
+    </div>
+    `;
+    return egresoHTML;
+}
+
+let eliminarEgreso = (id)=>{
+    let indiceEliminar = egresos.findIndex(egreso => egreso.id === id);
+    egresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarEgresos();
+}
+
+let agregarDato = ()=>{
+    let forma = document.forms['form-presupuesto'];
+    let tipo = forma['tipoMonto'];
+    let descripcion = forma['descriptionMonto'];
+    let valor = forma['valorMonto'];
+    if(descripcion.value !== '' && valor.value !== ''){
+        if(tipo.value === 'ingreso'){
+            ingresos.push( new Ingreso(descripcion.value, +valor.value));
+            cargarCabecero();
+            cargarIngresos();
+        }
+        else if(tipo.value === 'egreso'){
+           egresos.push( new Egreso(descripcion.value, +valor.value));
+           cargarCabecero();
+           cargarEgresos();
         }
     }
 }
 
-function generarError(){
-    let bodyPrincipal = document.querySelector('body');
-    let containerDiv = document.createElement('DIV');
-    containerDiv.classList.add('elementoImportanteMensaje');
-    let mensaje = document.createTextNode('Lo siento el Navegador no es compatible con Sesion storage');
-    console.log(typeof containerDiv)
-    containerDiv.appendChild(containerDiv.appendChild(mensaje));
-    bodyPrincipal.appendChild(containerDiv);
-}
 
-function main(){
+cargarApp();
 
-}
-
-if (typeof(Storage) !== 'undefined') {
-    main();
-} else {
-    generarError();
-}
